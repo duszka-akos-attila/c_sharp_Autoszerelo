2f4f8004ade262e1bce7d19c6db609cc24ff9761
tocsika7 <tocsamanatee@gmail.com>
2021-05-06 21:01:19 +0200

    Created a Model for the Jobs table. #13

---
+++ Server/API/Models/Job.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Models
+{
+    public class Job
+    {
+        [Key]
+        public long Id { get; set; }
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public String CarModel { get; set; }
+        [MaxLength(6)]
+        public String LicensePlate { get; set; }
+        public String Description { get; set; }
+
+    }
+}
