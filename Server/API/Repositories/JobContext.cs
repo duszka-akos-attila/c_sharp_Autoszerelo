55e603faa3b125dcf06919428b74680a3854a845
tocsika7 <tocsamanatee@gmail.com>
2021-05-07 20:39:50 +0200

    Created the context for Jobs table and setup the connection string #13

---
+++ Server/API/Repositories/JobContext.cs
@@ -0,0 +1,14 @@
+﻿using API.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Repositories
+{
+    public class JobContext : DbContext
+    {
+        public DbSet<Job> Jobs { get; set; }
+    }
+}
