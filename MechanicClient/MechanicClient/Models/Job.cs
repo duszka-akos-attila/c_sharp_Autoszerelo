5534d71fb0d003a2c6caa0beee3cd06614a522e3
Duszka Ákos Attila <43644528+duszka-akos-attila@users.noreply.github.com>
2021-05-15 14:22:54 +0200

    Created the model for the Mechanic Client

---
+++ MechanicClient/MechanicClient/Models/Job.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MechanicClient.Models
+{
+    class Job
+    {
+        private int Id { get; set; }
+
+        private string ClientName { get; set; }
+
+        private string CarModel { get; set; }
+
+        private string LicensePlate { get; set; }
+
+        private DateTime RegistrationDate { get; set; } 
+
+        private int Status { get; set; }
+
+        private string Description { get; set; }
+
+        public Job(int id, string clientName, string carModel, string licensePlate, DateTime registrationDate, int status, string description)
+        {
+            Id = id;
+            ClientName = clientName;
+            CarModel = carModel;
+            LicensePlate = licensePlate;
+            RegistrationDate = registrationDate;
+            Status = status;
+            Description = description;
+        }
+
+        public Job()
+        {
+        }
+    }
+}
