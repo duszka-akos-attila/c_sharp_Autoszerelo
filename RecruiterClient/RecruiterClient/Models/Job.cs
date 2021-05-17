b58cc0085d2432b1654356738f67e60e5ae61968
tocsika7 <tocsamanatee@gmail.com>
2021-05-17 19:44:15 +0200

    Created a DataProvider for the RecruiterClient #32

---
+++ RecruiterClient/RecruiterClient/Models/Job.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecruiterClient.Models
+{
+    class DatabaseJob
+    {
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public String CarModel { get; set; }
+        public String LicensePlate { get; set; }
+        public String Description { get; set; }
+
+    }
+}
---
+++ RecruiterClient/RecruiterClient/Models/JobList.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecruiterClient.Models
+{
+    class DatabaseJobList
+    {
+        public long Id { get; set; }
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public String CarModel { get; set; }
+        public String LicensePlate { get; set; }
+        public String Description { get; set; }
+        public String State { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
